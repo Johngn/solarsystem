7951cb1a1ada2e55dbcac3b43abadc0c9c40eef7
john <jgillan@gmail.com>
2020-06-19 12:41:56 +0200

    added 3D animations

--- solarsystem.py
+++ solarsystem.py
@@ -4,12 +4,12 @@ import matplotlib.pyplot as plt
 from astropy.time import Time
 from astropy.coordinates import get_body_barycentric_posvel
 from timeit import default_timer as timed
-from itertools import product
 from matplotlib import animation
+from mpl_toolkits.mplot3d import axes3d
 
-G = 4*np.pi**2                  # gravitational constant
-t = Time.now()                 # gets current time - required for astropy to get the locations of all planets at that time
-noutputs = 1000               # number of outputs
+G = 4*np.pi**2                  # gravitational constant - 4pi when distance is in AU and time is in years
+t = Time.now()                  # gets current time - required for astropy to get the locations of all planets at that time
+noutputs = 1500                 # number of outputs
 h = 0.01                        # integration timestep
 
 # names of bodies for astropy to search
@@ -19,10 +19,16 @@ bodies = ['sun', 'mercury', 'venus', 'earth', 'mars', 'jupiter', 'saturn', 'uran
 positions = [get_body_barycentric_posvel(bodies[i], t)[0].xyz for i in range(len(bodies))]
 velocities = [get_body_barycentric_posvel(bodies[i], t)[1].xyz*365.25 for i in range(len(bodies))]
 
-# masses of solar system bodies
+# masses of solar system bodies in solar masses
 M = np.array([1, 1.66e-7, 2.45e-6, 5.97e-6, 3.23e-7, 9.55e-4, 2.86e-4, 4.37e-5, 5.15e-5])
-W = np.hstack((positions, velocities)) # create array of positions and velocities together for integrator
+W0 = np.hstack((positions, velocities)) # create array of positions and velocities together for integrator
+# %%
+# this section creates a black hole and includes it in the simulation - optional
+BHM = 0.5           # mass of black hole in solar masses
+BHW = np.reshape(np.array([5,20,5,-1,-1,-0.5]), (1,6)) # x,y,z and vx,vy,vz of black hole
 
+M = np.append(M, BHM)  # append black hole mass to all masses
+W0 = np.vstack((W0, BHW)) # append position and velocity of black hole to other bodies
 # %%
 
 def force(W):
@@ -31,6 +37,7 @@ def force(W):
     for i in range(len(W)):
         dvdt = np.sum([-G*M[j]*(W[i,0:3]-W[j,0:3])/(np.linalg.norm(W[i,0:3]-W[j,0:3])**3) for j in range(len(W)) if j != i], axis=0)
         g[i] = np.hstack((W[i,3:6], dvdt))
+        
     return g
 
 def rungekutta(W):
@@ -50,78 +57,183 @@ def rungekutta(W):
     return W_total    
 
 timer = timed()
-W_total = rungekutta(W) # 6 dimensional phase space for each particle at each timestep
+W = rungekutta(W0) # 6 dimensional phase space for each particle at each timestep
 print(timed()-timer)
 
 # %%
-lim = 30
-fig, axes = plt.subplots(1, figsize=(10, 10))
+lim = 6
+fig, ax = plt.subplots(1, figsize=(10, 10))
 
-axes.set_xlabel('distance from sun (AU)')
-axes.set_ylabel('distance from sun (AU)')
-axes.set_xlim([-lim,lim])
-axes.set_ylim([-lim,lim])
+ax.set_xlabel('distance from sun (AU)')
+ax.set_ylabel('distance from sun (AU)')
+ax.set_xlim([-lim,lim])
+ax.set_ylim([-lim,lim])
 
-sun, = axes.plot([], [], 'o', label="Sun", color='yellow')
+sun, = ax.plot([], [], 'o', color='yellow', markersize=5)
+bh, = ax.plot([], [], 'o', label='Black hole', color='black', markersize=15)
 
-mercury, = axes.plot([], [], 'o', label="Mercury", color='darkorchid')
-venus, = axes.plot([], [], 'o', label="Venus", color='mediumseagreen')
-earth, = axes.plot([], [], 'o', label="Earth", color='deepskyblue')
-mars, = axes.plot([], [], 'o', label="Mars", color='indianred')
-jupiter, = axes.plot([], [], 'o', label="Jupiter", color='darkgoldenrod')
-saturn, = axes.plot([], [], 'o', label="Saturn", color='darkkhaki')
-uranus, = axes.plot([], [], 'o', label="Uranus", color='lightblue')
-neptune, = axes.plot([], [], 'o', label="Neptune", color='steelblue')
+merc, = ax.plot([], [], 'o', label="Mercury", color='plum', markersize=5)
+ven, = ax.plot([], [], 'o', label="Venus", color='mediumaquamarine', markersize=8)
+earth, = ax.plot([], [], 'o', label="Earth", color='deepskyblue', markersize=8)
+mars, = ax.plot([], [], 'o', label="Mars", color='lightcoral', markersize=8)
+jup, = ax.plot([], [], 'o', label="Jupiter", color='darkgoldenrod', markersize=12)
+sat, = ax.plot([], [], 'o', label="Saturn", color='darkkhaki', markersize=12)
+urn, = ax.plot([], [], 'o', label="Uranus", color='lightblue', markersize=10)
+nep, = ax.plot([], [], 'o', label="Neptune", color='steelblue', markersize=10)
 
-mercuryline, = axes.plot([], [], linewidth=1, color='darkorchid')
-venusline, = axes.plot([], [], linewidth=1, color='mediumseagreen')
-earthline, = axes.plot([], [], linewidth=1, color='deepskyblue')
-marsline, = axes.plot([], [], linewidth=1, color='indianred')
-jupiterline, = axes.plot([], [], linewidth=1, color='darkgoldenrod')
-saturnline, = axes.plot([], [], linewidth=1, color='darkkhaki')
-uranusline, = axes.plot([], [], linewidth=1, color='lightblue')
-neptuneline, = axes.plot([], [], linewidth=1, color='steelblue')
 
-text = axes.text(-lim+1, lim-2, lim-1, fontsize=15)
+sunl, = ax.plot([], [], linewidth=1, color='yellow')
+bhl, = ax.plot([], [], linewidth=1, color='black')
 
-axes.legend()
+mercl, = ax.plot([], [], linewidth=1, color='plum')
+venl, = ax.plot([], [], linewidth=1, color='mediumaquamarine')
+earthl, = ax.plot([], [], linewidth=1, color='deepskyblue')
+marsl, = ax.plot([], [], linewidth=1, color='lightcoral')
+jupl, = ax.plot([], [], linewidth=1, color='darkgoldenrod')
+satl, = ax.plot([], [], linewidth=1, color='darkkhaki')
+urnl, = ax.plot([], [], linewidth=1, color='lightblue')
+nepl, = ax.plot([], [], linewidth=1, color='steelblue')
 
-times = np.linspace(0, noutputs*h, noutputs) # for displaying time elapsed in plot
+text = ax.text(-lim+1, lim-1, s='', fontsize=15)
+# ax.grid()
+ax.legend()
+
+times = np.linspace(0, noutputs*h, noutputs)+2020 # for displaying time elapsed in plot
 
 def animate(i):
-    sun.set_data(W_total[i,0,0], W_total[i,0,1])
+    sun.set_data(W[i,0,0], W[i,0,1])
+    bh.set_data(W[i,9,0], W[i,9,1])
     
-    mercury.set_data(W_total[i,1,0], W_total[i,1,1])
-    venus.set_data(W_total[i,2,0], W_total[i,2,1])
-    earth.set_data(W_total[i,3,0], W_total[i,3,1])
-    mars.set_data(W_total[i,4,0], W_total[i,4,1])
-    jupiter.set_data(W_total[i,5,0], W_total[i,5,1])
-    saturn.set_data(W_total[i,6,0], W_total[i,6,1])
-    uranus.set_data(W_total[i,7,0], W_total[i,7,1])
-    neptune.set_data(W_total[i,8,0], W_total[i,8,1])
+    merc.set_data(W[i,1,0], W[i,1,1])
+    ven.set_data(W[i,2,0], W[i,2,1])
+    earth.set_data(W[i,3,0], W[i,3,1])
+    mars.set_data(W[i,4,0], W[i,4,1])
+    jup.set_data(W[i,5,0], W[i,5,1])
+    sat.set_data(W[i,6,0], W[i,6,1])
+    urn.set_data(W[i,7,0], W[i,7,1])
+    nep.set_data(W[i,8,0], W[i,8,1])
     
-    mercuryline.set_data(W_total[0:i,1,0], W_total[0:i,1,1])
-    venusline.set_data(W_total[0:i,2,0], W_total[0:i,2,1])
-    earthline.set_data(W_total[0:i,3,0], W_total[0:i,3,1])
-    marsline.set_data(W_total[0:i,4,0], W_total[0:i,4,1])
-    jupiterline.set_data(W_total[0:i,5,0], W_total[0:i,5,1])
-    saturnline.set_data(W_total[0:i,6,0], W_total[0:i,6,1])
-    uranusline.set_data(W_total[0:i,7,0], W_total[0:i,7,1])
-    neptuneline.set_data(W_total[0:i,8,0], W_total[0:i,8,1])
+    sunl.set_data(W[0:i,0,0], W[0:i,0,1])
+    bhl.set_data(W[0:i,9,0], W[0:i,9,1])
     
-    text.set_text('{:.2f} Years'.format(times[i]))
-    return sun, mercury, venus, earth, mars, jupiter, saturn, uranus, neptune, mercuryline, venusline, earthline, marsline, jupiterline, saturnline, uranusline, neptuneline, text
+    mercl.set_data(W[0:i,1,0], W[0:i,1,1])
+    venl.set_data(W[0:i,2,0], W[0:i,2,1])
+    earthl.set_data(W[0:i,3,0], W[0:i,3,1])
+    marsl.set_data(W[0:i,4,0], W[0:i,4,1])
+    jupl.set_data(W[0:i,5,0], W[0:i,5,1])
+    satl.set_data(W[0:i,6,0], W[0:i,6,1])
+    urnl.set_data(W[0:i,7,0], W[0:i,7,1])
+    nepl.set_data(W[0:i,8,0], W[0:i,8,1])
     
-im_ani = FuncAnimation(fig, animate, frames=noutputs, interval=1, blit=True)
+    text.set_text('{:.0f}'.format(times[i]))
+    
+    return sun,merc,ven,earth,mars,jup,sat,urn,nep,mercl,venl,earthl,marsl,jupl,satl,urnl,nepl,bh,sunl,bhl,text
+    
+im_ani = animation.FuncAnimation(fig, animate, frames=noutputs, interval=1, blit=True)
+# %%
+lim = 6
+fig = plt.figure(figsize=(10,10))
+ax = fig.add_subplot(111, projection='3d')
+
+ax.set_xlabel('distance from sun (AU)')
+ax.set_ylabel('distance from sun (AU)')
+ax.set_zlabel('distance from sun (AU)')
+ax.set_xlim([-lim,lim])
+ax.set_ylim([-lim,lim])
+ax.set_zlim([-lim,lim])
+
+sun, = ax.plot([],[],[], 'o', color='yellow', markersize=5)
+bh, = ax.plot([],[],[], 'o', label="Black hole", color='black', markersize=15)
+
+merc, = ax.plot([],[],[], 'o', label="Mercury", color='plum', markersize=4)
+ven, = ax.plot([],[],[], 'o', label="Venus", color='mediumaquamarine', markersize=6)
+earth, = ax.plot([],[],[], 'o', label="Earth", color='deepskyblue', markersize=6)
+mars, = ax.plot([],[],[], 'o', label="Mars", color='lightcoral', markersize=6)
+jup, = ax.plot([],[],[], 'o', label="Jupiter", color='darkgoldenrod', markersize=10)
+sat, = ax.plot([],[],[], 'o', label="Saturn", color='darkkhaki', markersize=10)
+urn, = ax.plot([],[],[], 'o', label="Uranus", color='lightblue', markersize=8)
+nep, = ax.plot([],[],[], 'o', label="Neptune", color='steelblue', markersize=8)
+
+
+sunl, = ax.plot([],[],[], linewidth=0.5, color='yellow')
+bhl, = ax.plot([],[],[], linewidth=0.5, color='black')
+
+mercl, = ax.plot([],[],[], linewidth=0.5, color='plum')
+venl, = ax.plot([],[],[], linewidth=0.5, color='mediumaquamarine')
+earthl, = ax.plot([],[],[], linewidth=0.5, color='deepskyblue')
+marsl, = ax.plot([],[],[], linewidth=0.5, color='lightcoral')
+jupl, = ax.plot([],[],[], linewidth=0.5, color='darkgoldenrod')
+satl, = ax.plot([],[],[], linewidth=0.5, color='darkkhaki')
+urnl, = ax.plot([],[],[], linewidth=0.5, color='lightblue')
+nepl, = ax.plot([],[],[], linewidth=0.5, color='steelblue')
+
+text = ax.text(-lim+1, lim-.0, lim-1, s='', fontsize=15)
+ax.legend()
+
+times = np.linspace(0, noutputs*h, noutputs)+2020 # for displaying time elapsed in plot
+
+def animate(i):
+    sun.set_data(W[i,0,0], W[i,0,1])
+    bh.set_data(W[i,9,0], W[i,9,1])
+    
+    merc.set_data(W[i,1,0], W[i,1,1])
+    ven.set_data(W[i,2,0], W[i,2,1])
+    earth.set_data(W[i,3,0], W[i,3,1])
+    mars.set_data(W[i,4,0], W[i,4,1])
+    jup.set_data(W[i,5,0], W[i,5,1])
+    sat.set_data(W[i,6,0], W[i,6,1])
+    urn.set_data(W[i,7,0], W[i,7,1])
+    nep.set_data(W[i,8,0], W[i,8,1])
+    
+    sun.set_3d_properties(W[i,0,2])
+    bh.set_3d_properties(W[i,9,2])
+    
+    merc.set_3d_properties(W[i,1,2])
+    ven.set_3d_properties(W[i,2,2])
+    earth.set_3d_properties(W[i,3,2])
+    mars.set_3d_properties(W[i,4,2])
+    jup.set_3d_properties(W[i,5,2])
+    sat.set_3d_properties(W[i,6,2])
+    urn.set_3d_properties(W[i,7,2])
+    nep.set_3d_properties(W[i,8,2])
+    
+    sunl.set_data(W[0:i,0,0], W[0:i,0,1])
+    bhl.set_data(W[0:i,9,0], W[0:i,9,1])
+    
+    mercl.set_data(W[0:i,1,0], W[0:i,1,1])
+    venl.set_data(W[0:i,2,0], W[0:i,2,1])
+    earthl.set_data(W[0:i,3,0], W[0:i,3,1])
+    marsl.set_data(W[0:i,4,0], W[0:i,4,1])
+    jupl.set_data(W[0:i,5,0], W[0:i,5,1])
+    satl.set_data(W[0:i,6,0], W[0:i,6,1])
+    urnl.set_data(W[0:i,7,0], W[0:i,7,1])
+    nepl.set_data(W[0:i,8,0], W[0:i,8,1])
+    
+    sunl.set_3d_properties(W[0:i,0,2])
+    bhl.set_3d_properties(W[0:i,9,2])
+    
+    mercl.set_3d_properties(W[0:i,1,2])
+    venl.set_3d_properties(W[0:i,2,2])
+    earthl.set_3d_properties(W[0:i,3,2])
+    marsl.set_3d_properties(W[0:i,4,2])
+    jupl.set_3d_properties(W[0:i,5,2])
+    satl.set_3d_properties(W[0:i,6,2])
+    urnl.set_3d_properties(W[0:i,7,2])
+    nepl.set_3d_properties(W[0:i,8,2])
+    
+    text.set_text('{:.0f}'.format(times[i]))
+    
+    return sun,merc,ven,earth,mars,jup,sat,urn,nep,mercl,venl,earthl,marsl,jupl,satl,urnl,nepl,bh,sunl,bhl,text
+    
+im_ani = animation.FuncAnimation(fig, animate, frames=noutputs, interval=1, blit=True)
 # %%
 plt.rcParams['animation.ffmpeg_path'] = '/usr/bin/ffmpeg'
 
-f = f'vid/solarsystemlarge.mp4' 
-writervideo = animation.FFMpegWriter(fps=40) # ffmpeg must be installed
-im_ani.save(f, writer=writervideo)
+writervideo = animation.FFMpegWriter(fps=60) # ffmpeg must be installed
+im_ani.save('solarsysteminnerBH3D.mp4', writer=writervideo)
 
 # %%
-
+# this code calulates the total energy of the system - should be relatively constant
 def time(m, v):
     return 0.5 * m * v
     
